d7a0a40173b1e668ba1b55d43c8f44157b0d37a8
MdRehanKachhi <mdrehankachhi786@gmail.com>
2026-04-25 16:42:59 +0530

    fix allowed hosts

--- backend/academic_erp/urls.py
+++ backend/academic_erp/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🎉 Academic ERP is Live!")
+
 
 urlpatterns = [
+    path('',home),
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
